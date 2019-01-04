6e37c8a3826f29a16e947c72da2b682bfa093fd8
Emerald Chen <ahyuancy@gmail.com>
2019-01-04 15:09:42 +1300

    Update .js

--- .js
+++ .js
@@ -1,7 +1,9 @@
+/**************  Solution #1   ************************************************************************************/
+
 function truthCheck(collection, pre) {
   let counter = 0; // Create a counter to count how many are true
   for(let i = 0; i < collection.length; i++){
-    if(collection[i][pre]){
+    if(collection[i][pre]){ // Access object properties through [] notation
       counter++;
     }
   }
@@ -12,6 +14,28 @@ function truthCheck(collection, pre) {
   }
   return pre;
 }
-
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, 
 {"user": "Po", "sex": "female"}], "sex"); // return true
+
+
+/**************  Solution #2   ************************************************************************************/
+
+function truthCheck2(collection, pre) {
+  let counter = 0;
+  for(let obj in collection){ // Loop through indices of the passed array
+  // If it is has property and value is truthy
+    if(collection[obj].hasOwnProperty(pre) && Boolean(collection[obj][pre])){
+      counter++;
+    }
+  }
+  // Outside the loop, check to see if all of them are true and return true or false
+  return counter === collection.length;
+}
+
+
+
+
+
+
+
+
